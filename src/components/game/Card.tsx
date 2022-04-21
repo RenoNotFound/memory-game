4bfbabe3fffab6ccae597e77b648693ef5adf4e4
Renato Gamper <gamper.renato@gmail.com>
2022-04-21 16:06:35 +0200

    create card component and display cards and set choices

---
+++ src/components/game/Card.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import {
+  ICard,
+  Cards,
+  activeCardsSelector,
+  addActiveCard,
+  resetActiveCards,
+} from "../../features/cards/cardsSlice";
+import { useSelector, useDispatch } from "react-redux";
+
+import "./game.css";
+
+interface CardProps {
+  card: ICard;
+  matched: boolean;
+  flipped: boolean;
+  greenHighlight: string;
+  firstChoice: ICard | null;
+  secondChoice: ICard | null;
+  setFirstChoice: React.Dispatch<React.SetStateAction<ICard | null>>;
+  setSecondChoice: React.Dispatch<React.SetStateAction<ICard | null>>;
+}
+
+const Card: React.FC<CardProps> = ({
+  card,
+  flipped,
+  greenHighlight,
+  firstChoice,
+  secondChoice,
+  setFirstChoice,
+  setSecondChoice,
+}): JSX.Element => {
+  const handleClick = (): void => {
+    if (!(firstChoice && secondChoice)) {
+      if (card !== firstChoice) {
+        firstChoice ? setSecondChoice(card) : setFirstChoice(card);
+      }
+    }
+  };
+
+  return (
+    <div className="cards-container" onClick={handleClick}>
+      {flipped ? (
+        <img
+          className={`${greenHighlight} ${card.matched ? "hidden" : ""}`}
+          src={card.url}
+          alt="cats"
+          width="150"
+          height="150"
+        />
+      ) : (
+        <img src="/back-img.png" alt="cats" width="150" height="150" />
+      )}
+    </div>
+  );
+};
+
+export default Card;
