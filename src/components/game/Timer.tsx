150af421f48a3e31d991fde671e08d5307e6a699
Renato Gamper <gamper.renato@gmail.com>
2022-04-21 18:55:55 +0200

    Create countdown timer and navigate to score page when it hits 0

---
+++ src/components/game/Timer.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./game.css";
+
+interface TimerProps {
+  time: number;
+  score: number;
+}
+
+interface State {
+  time: number;
+  seconds: number;
+  minutes: number;
+}
+
+const Timer: React.FC<TimerProps> = ({ time, score }) => {
+  const navigate = useNavigate();
+
+  const [state, setState] = useState<State>({
+    time,
+    seconds: time - Math.floor((time - 1) / 60) * 60 - 1,
+    minutes: Math.floor((time - 1) / 60),
+  });
+
+  useEffect(() => {
+    if (state.time === 0) {
+      navigate(`/score?points=${score}`);
+    }
+    setTimeout(() => {
+      if (state.time === 0) {
+        return;
+      }
+
+      setState({
+        time: state.time - 1,
+        seconds: state.time - Math.floor((state.time - 1) / 60) * 60 - 1,
+        minutes: Math.floor((state.time - 1) / 60),
+      });
+    }, 1000);
+  }, [navigate, state.time]);
+
+  return (
+    <span className="col-3">{`${state.minutes}: ${
+      state.seconds < 10 ? `0${state.seconds}` : `${state.seconds}`
+    }`}</span>
+  );
+};
+
+export default Timer;
